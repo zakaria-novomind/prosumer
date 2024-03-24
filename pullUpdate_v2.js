f0ab72eee8612ace36aa83abcabf275bd480942e
zakaria-novomind <zzermani@novomind.com>
2024-03-24 14:56:43 +0100

    update in welcome page

--- pullUpdate_v2.js
+++ pullUpdate_v2.js
@@ -5,14 +5,20 @@ const serverHost = '52.86.213.65';
 const username = 'ec2-user';
 const privateKeyPath = 'priv-key.txt';
 
+
+// Define the Docker command you want to execute
+const dockerPullCommand = 'sudo docker-compose pull prosumerapp';
+const dockerInstallCommand = 'sudo docker-compose up -d prosumerapp';
+const testcmd = 'sudo docker-compose -f nginx/docker-compose.yml down'
 const conn = new Client();
+
 
 conn.on('ready', () => {
   console.log('Connected to server');
 
   // Execute commands after successful connection (optional)
   // You can replace this with your desired commands
-  conn.exec('sudo touch filee', (err, stream) => {
+  conn.exec(testcmd, (err, stream) => {
     if (err) {
       console.error(err);
       return;
